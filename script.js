d9398a0cbc650a47c55e61d63624799a9ba3b5d2
Mateusz Siewiera <mateusz.siewiera@gmail.com>
2023-03-27 18:39:23 +0200

    add js exercise script

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("For Loop:")
+var word = "Hello"
+for(var i=0;i<5; i++){
+    console.log(word)
+}
+var index = 0
+console.log("While Loop:")
+while(index<5){
+    console.log(word)
+    index++
+}

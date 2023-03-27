531a8a434f319903559573021c1d9cc4e3d9188d
Mateusz Siewiera <mateusz.siewiera@gmail.com>
2023-03-27 21:27:22 +0200

    test function creation

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-var fname = prompt("Type your first name: ")
-var surename = prompt("Type your last name: ")
-var age = prompt("Type your age: ")
-var tall = prompt("Type your height: ")
-var petName = prompt("Type your pet name: ")
-console.log(petName[petName.length-1].toLowerCase())
-if(fname[0].toLowerCase() == surename[0].toLowerCase() && (age>20 && age<30) && tall>=170 && petName[petName.length-1].toLowerCase() == "y"){
-    console.log('Hello spy comrade!')
-}else{
-    console.log('Nothing to find here!')
+function formal(title="Sir",name="Mateusz"){
+    return title+" "+name
 }
+var a = formal()
+console.log("Hello "+a)
+
+"Welcome "+formal()

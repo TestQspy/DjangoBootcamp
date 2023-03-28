42ced2b782cebdd070f3da282366b3fc5af982e3
Mateusz Siewiera <mateusz.siewiera@gmail.com>
2023-03-28 02:26:14 +0200

    add some js object manipulation

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-var users = []
-var fname = ""
-while (true) {
-    var choice = prompt("Type one of the specific action: \nadd - to add new user to the array \nremove - to remove specific user from the array \ndisplay - to print out all of the users \nquit - to end the loop")
-    if (choice.toLowerCase() === "quit") {
-        break
-    } else if (choice.toLowerCase() === "add"){
-        fname = prompt("Please type a first name to add a user: ")
-        users.push(fname)
-    } else if (choice.toLowerCase() === "remove"){
-        fname = prompt("Please type a first name to remove a user: ")
-        var index = users.indexOf(fname);
-        if (index > -1) {
-          users.splice(index, 1); // remove the element at the specified index
-        }
-    } else if (choice.toLowerCase()=="display"){
-        console.log(users)
+var emplyee = {
+    name: "John Smith",
+    job: "Programmer",
+    age: 31,
+    nameLength: function(){
+        console.log(this.name.length)
+    },
+    lastName: function(){
+        console.log(this.name.split(" ")[1])
     }
 }
+
+for (key in emplyee) {
+    alert(key+" is "+emplyee[key])
+};
+

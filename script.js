550c286e4847aa8e520b1b87e6aed78775e2a365
Mateusz Siewiera <mateusz.siewiera@gmail.com>
2023-03-28 13:00:03 +0200

    add some funk with DOM manipulation

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-var emplyee = {
-    name: "John Smith",
-    job: "Programmer",
-    age: 31,
-    nameLength: function(){
-        console.log(this.name.length)
-    },
-    lastName: function(){
-        console.log(this.name.split(" ")[1])
+var header = document.querySelector("h1")
+
+function randomColor() {
+    var letters = "0123456789ABCDEF"
+    var color = "#"
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random()*16)]
     }
+    return color
 }
 
-for (key in emplyee) {
-    alert(key+" is "+emplyee[key])
-};
+function changeHeaderColor(){
+    colorInput = randomColor()
+    header.style.color = colorInput
+}
 
+// setInterval('changeHeaderColor()',500)
+header.addEventListener("click",changeHeaderColor)
